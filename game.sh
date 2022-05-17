2122e76d2c2eec85015d5d05aeab45bd43b30d29
Kanstantsin Klimov <klimovkostya5@gmail.com>
2022-05-17 16:08:28 +0300

    Initial commit

---
+++ game.sh
@@ -0,0 +1,42 @@
+echo -e "\n NumberJack \n"
+ch=0
+while [ $ch -ne 3 ]; do
+    echo  "  
+         PLAY : Hit 1 and enter.
+         HELP : Hit 2 and enter.
+         EXIT : Hit 3 and enter.
+         "
+    read -p "Enter your choice : " ch
+    if [ $ch -eq 1 ];then
+    x=0 ;c=0 ;p=0
+    read -p "Enter any number between 0 and 9 : " n
+    while [ $c -eq 0 ];
+    do
+        x=11; r=($(shuf -i 0-9 -n 10))
+        echo "${r[@]} "; for i in {1..10}; do
+            a[$i]=$i
+        done
+        echo "${a[@]} "
+        read -t 5 -p "Enter the index of your number : " x
+    if [[ $? -gt 128 ]]; then
+    c=1
+    break
+    fi
+if [ ${r[$(($x))-1]} -eq $n ];then
+            echo "Great"
+    ((p=p+1))
+        else
+            c=1
+            break
+        fi
+    done
+    elif [ $ch -eq 2 ];then
+        echo "HELP: INSTRUCTIONS TO PLAY THE GAME. "
+else
+        break
+fi
+if [ $c -eq 1 ];then
+            echo -e "\nGAME OVER\n"
+            echo "You scored $p points"
+fi
+        done
