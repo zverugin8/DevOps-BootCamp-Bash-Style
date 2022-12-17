7e8738d4b40bb5ccbe12353160da2de3db0a1e33
Siarhei Saroka <siarhei_saroka@epam.com>
2022-12-17 12:41:26 +0300

    fix 3.1

--- game.sh
+++ game.sh
@@ -13,10 +13,10 @@ while [ "$ch" -ne 3 ]; do
       "
   read -r -p "Enter your choice : " ch
   if [ "$ch" -eq 1 ];then
-      x=0 
-      c=0 
-      p=0
-      read -r -p "Enter any number between 0 and 9 : " n
+    x=0 
+    c=0 
+    p=0
+    read -r -p "Enter any number between 0 and 9 : " n
     while [ "$c" -eq 0 ]; do
       x=11; 
       r=() 
@@ -33,7 +33,7 @@ while [ "$ch" -ne 3 ]; do
         c=1
         break
       fi
-      if [ "${r[(($x))-1]}" -eq "$n" ];then
+      if [ "${r[(($x))-1]}" -eq "$n" ]; then
         echo "Great"
         ((p=p+1))
       else
@@ -46,7 +46,7 @@ while [ "$ch" -ne 3 ]; do
   else
     break
   fi
-  if [ "$c" -eq 1 ];then
+  if [ "$c" -eq 1 ]; then
     echo -e "\nGAME OVER\n"
     echo "You scored $p points"
   fi
